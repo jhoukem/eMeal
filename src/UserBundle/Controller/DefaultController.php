dcfd61d2caef1ee09aed3f406e7b2aa3c0ef9eca
Ludovic <ludovickalkadebidine@gmail.com>
2017-03-24 14:23:51 -0400

    mise a jour  CSS FOSUserBundle

--- src/UserBundle/Controller/DefaultController.php
+++ src/UserBundle/Controller/DefaultController.php
@@ -4,11 +4,23 @@ namespace UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+
 class DefaultController extends Controller
 {
-    public function indexAction()
+	/* public function indexAction()
     {
-        return $this->render('UserBundle:Default:index.html.twig');
-    }
+        $em = $this->getDoctrine()->getManager();
+        $utilisateurs = $em->getRepository('UserBundle:User')->findAll();
+        
+        return $this->render('UserBundle::layout.html.twig', array('utilisateurs' => $utilisateurs));
+    }*/
+    public function utilisateurAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+		$utilisateur = $em->getRepository('UserBundle:User')->find($id);
+
+		return $this->render('UserBundle:Default:login.html.twig'); 
+		
+	}
     
 }
